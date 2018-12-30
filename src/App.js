6f752fa4e7d9b144cd2aa2e4121c1832754fc0b3
littlestone <littlestone@suboat.com>
2018-12-30 15:06:43 +0800

    feat: initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Button } from './components/index'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>sksd</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
